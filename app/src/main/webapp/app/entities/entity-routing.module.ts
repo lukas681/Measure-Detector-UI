a2007afc6298decb8fef5d31c2f9d51b4d5b676b
Lukas Retschmeier <lukas.retschmeier@atos.net>
2021-11-27 13:05:33 +0100

    Started to reorganize the page entry

--- app/src/main/webapp/app/entities/entity-routing.module.ts
+++ app/src/main/webapp/app/entities/entity-routing.module.ts
@@ -4,6 +4,31 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'project',
+        data: { pageTitle: 'measureDetectorApp.project.home.title' },
+        loadChildren: () => import('./project/project.module').then(m => m.ProjectModule),
+      },
+      {
+        path: 'edition',
+        data: { pageTitle: 'measureDetectorApp.edition.home.title' },
+        loadChildren: () => import('./edition/edition.module').then(m => m.EditionModule),
+      },
+      {
+        path: 'page',
+        data: { pageTitle: 'measureDetectorApp.page.home.title' },
+        loadChildren: () => import('./page/page.module').then(m => m.PageModule),
+      },
+      {
+        path: 'measure-box',
+        data: { pageTitle: 'measureDetectorApp.measureBox.home.title' },
+        loadChildren: () => import('./measure-box/measure-box.module').then(m => m.MeasureBoxModule),
+      },
+      {
+        path: 'tag',
+        data: { pageTitle: 'measureDetectorApp.tag.home.title' },
+        loadChildren: () => import('./tag/tag.module').then(m => m.TagModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
---
+++ doc/measure-detector-docs/docs/architecture.md
@@ -0,0 +1,17 @@
+# Architectural Overview
+
+## JHipster
+
+## Data Model
+
+The datamodel can be generated for the corresponding JDL file which is located in the /jdl folder on the top level. 
+
+Further information can be found at the official documentation: [JDL Documentation](http://easynode.github.io/jdl/)
+
+in order to generate new entities just modify the jdl file and call
+
+    $ jhipster jdl ./jdl/jdl.jh
+
+The output should be something like this:
+
+![Entity Update](img/entity-update.png)
